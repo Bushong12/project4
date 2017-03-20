3f28fc16c04a380aa9a19b47dd1c0b2b480f5f97
gbushong <graceebushong@gmail.com>
2017-03-19 21:34:56 -0400

    error checking for argc in main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 
 int main(int argc, char *argv[]){
   Config run;
+  if (argc != 2) {
+  	cout << argv[0] << ": Incorrect arguments!" << endl;
+  	cout << "usage: " << argv[0] << " <configfile>" << endl;
+  	exit(1);
+  }
   string fname = argv[1];
   run.parse_input_file(fname);
   run.parse_search_file();
