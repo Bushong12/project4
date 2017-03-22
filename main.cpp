d7abbc58a9cddbe7a89c2fdb2f734f379a6103b8
Grace Elisabeth Bushong <gbushong@student02.cse.nd.edu>
2017-03-22 15:16:39 -0400

    some threading

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <string>
 #include "config.h"
+#include <pthread>
+#include <sys/types.h>
+
+#define MAX_THREAD 1000
 
 int main(int argc, char *argv[]){
+  pthread_t *threads;
+  thread_args_struct *t;
+
   Config run;
   if (argc != 2) {
   	cout << argv[0] << ": Incorrect arguments!" << endl;
@@ -11,6 +18,10 @@ int main(int argc, char *argv[]){
   }
   string fname = argv[1];
   run.parse_input_file(fname);
+  if ((run.get_fetch_threads() < 1) || (run.get_parse_threads() < 1) || (run.get_fetch_threads() > MAX_THREAD) || (run.get_parse_threads() > MAX_THREAD)) {
+	printf("The number of threads should be between 1 and %d.\n", MAX_THREAD);
+	exit(1);
+  }
   run.parse_search_file();
   run.parse_site_file();
   run.get_site();
