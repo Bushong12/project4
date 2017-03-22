41254a390f865d0b2fc76b3b62206515e8518cdd
Grace Elisabeth Bushong <gbushong@student02.cse.nd.edu>
2017-03-22 15:24:58 -0400

    addig threading vars

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
 #include "config.h"
-#include <pthread>
+#include <pthread.h>
 #include <sys/types.h>
 
 #define MAX_THREAD 1000
 
 int main(int argc, char *argv[]){
-  pthread_t *threads;
-  thread_args_struct *t;
+  pthread_t *threads_f;
+  pthread_t *threads_p;
 
   Config run;
   if (argc != 2) {
@@ -18,10 +18,15 @@ int main(int argc, char *argv[]){
   }
   string fname = argv[1];
   run.parse_input_file(fname);
-  if ((run.get_fetch_threads() < 1) || (run.get_parse_threads() < 1) || (run.get_fetch_threads() > MAX_THREAD) || (run.get_parse_threads() > MAX_THREAD)) {
+  int num_fetch = run.get_fetch_threads();
+  int num_parse = run.get_parse_threads();
+  if ((num_fetch < 1) || (num_parse < 1) || (num_fetch > MAX_THREAD) || (num_parse > MAX_THREAD)) {
 	printf("The number of threads should be between 1 and %d.\n", MAX_THREAD);
 	exit(1);
   }
+  threads_f = (pthread_t *)malloc(num_fetch*sizeof(*threads_f));
+  threads_p = (pthread_t *)malloc(num_parse*sizeof(*threads_p)); 
+ 
   run.parse_search_file();
   run.parse_site_file();
   run.get_site();
