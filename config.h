e1e90944811e668839199cee25087d0f00839b8a
Grace Elisabeth Bushong <gbushong@student02.cse.nd.edu>
2017-03-27 12:40:09 -0400

    Writes to files more correctly

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ using namespace std;
 queue<string> queue_sites;
 queue<string> queue_search;
 queue<string> queue_data;
-queue<pair<string, int> > queue_word_counts;
+queue<pair<string, int> > queue_word_counts;    // stores the search word and the count on a site
 vector<string> searches;
 vector<int> searches_counts;
 vector<string> sites;
@@ -28,107 +28,107 @@ pthread_mutex_t mutex;
 int numFile = 0;
 
 struct MemoryStruct {
-  char *memory;
-  size_t size;
+    char *memory;
+    size_t size;
 };
 
 static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp){
-  size_t realsize = size * nmemb;
-  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+    size_t realsize = size * nmemb;
+    struct MemoryStruct *mem = (struct MemoryStruct *)userp;
 
-  mem->memory = (char*)realloc(mem->memory, mem->size + realsize + 1);
-  if(mem->memory == NULL) {
-    /* out of memory! */
-    printf("not enough memory (realloc returned NULL)\n");
-    return 0;
-  }
+    mem->memory = (char*)realloc(mem->memory, mem->size + realsize + 1);
+    if(mem->memory == NULL) {
+        /* out of memory! */
+        printf("not enough memory (realloc returned NULL)\n");
+        return 0;
+    }
 
-  memcpy(&(mem->memory[mem->size]), contents, realsize);
-  mem->size += realsize;
-  mem->memory[mem->size] = 0;
+    memcpy(&(mem->memory[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
 
-  return realsize;
+    return realsize;
 }
 
 class Config {
- public:
-  Config();
-  int get_fetch_threads();
-  int get_parse_threads();
-  int get_period_fetch();
-  void parse_input_file(string fname);
-  void parse_search_file();
-  void parse_site_file();
-  //  int write_to_output(string num);
-  void push_search_to_queue();
- private:
-  int period_fetch;
-  int num_fetch;
-  int num_parse;
-  string search_file;
-  string site_file;
+    public:
+        Config();
+        int get_fetch_threads();
+        int get_parse_threads();
+        int get_period_fetch();
+        void parse_input_file(string fname);
+        void parse_search_file();
+        void parse_site_file();
+        //  int write_to_output(string num);
+        void push_search_to_queue();
+    private:
+        int period_fetch;
+        int num_fetch;
+        int num_parse;
+        string search_file;
+        string site_file;
 };
 
 //set default arguments
 Config::Config(){
-  period_fetch = 180;
-  num_fetch = 1;
-  num_parse = 1;
-  search_file = "Search.txt";
-  site_file = "Sites.txt";
+    period_fetch = 180;
+    num_fetch = 1;
+    num_parse = 1;
+    search_file = "Search.txt";
+    site_file = "Sites.txt";
 }
 
 int Config::get_fetch_threads(){
-  return num_fetch;
+    return num_fetch;
 }
 
 int Config::get_parse_threads(){
-  return num_parse;
+    return num_parse;
 }
 
 int Config::get_period_fetch(){
-  return period_fetch;
+    return period_fetch;
 }
 
 void Config::parse_input_file(string fname){
-  ifstream infile(fname.c_str());
-  string line;
-  while(getline(infile, line)){
-    size_t found = line.find("=");
-    string param, arg;
-    for(size_t i = 0; i < found; i++){
-      param = param + line[i];
+    ifstream infile(fname.c_str());
+    string line;
+    while(getline(infile, line)){
+        size_t found = line.find("=");
+        string param, arg;
+        for(size_t i = 0; i < found; i++){
+            param = param + line[i];
+        }
+        for(size_t i = found + 1; i < line.size(); i++){
+            arg = arg + line[i];
+        }
+        if(param == "PERIOD_FETCH")
+            period_fetch = atoi(arg.c_str());
+        else if(param == "NUM_FETCH")
+            num_fetch = atoi(arg.c_str());
+        else if(param == "NUM_PARSE")
+            num_parse = atoi(arg.c_str());
+        else if(param == "SEARCH_FILE")
+            search_file = arg;
+        else if(param == "SITE_FILE")
+            site_file = arg;
     }
-    for(size_t i = found + 1; i < line.size(); i++){
-      arg = arg + line[i];
-    }
-    if(param == "PERIOD_FETCH")
-      period_fetch = atoi(arg.c_str());
-    else if(param == "NUM_FETCH")
-      num_fetch = atoi(arg.c_str());
-    else if(param == "NUM_PARSE")
-      num_parse = atoi(arg.c_str());
-    else if(param == "SEARCH_FILE")
-      search_file = arg;
-    else if(param == "SITE_FILE")
-      site_file = arg;
-  }
 }
 
 void Config::parse_search_file(){
-  ifstream infile(search_file.c_str());
-  string line;
-  while(getline(infile, line)){
-    searches.push_back(line);
-  }
+    ifstream infile(search_file.c_str());
+    string line;
+    while(getline(infile, line)){
+        searches.push_back(line);
+    }
 }
 
 void Config::parse_site_file(){
-  ifstream infile(site_file.c_str());
-  string line;
-  while(getline(infile, line)){
-    sites.push_back(line);
-  }
+    ifstream infile(site_file.c_str());
+    string line;
+    while(getline(infile, line)){
+        sites.push_back(line);
+    }
 }
 
 void Config::push_search_to_queue() {
@@ -138,137 +138,149 @@ void Config::push_search_to_queue() {
 }
 
 void write_to_output(string name){
-  pthread_mutex_lock(&mutex);
-  while(queue_word_counts.empty()){ // nothing to write
-    pthread_cond_wait(&third_signal, &mutex);
-  }
-
-  cout << "writing to output"<<endl;
-
-  //int limit = sites.size() * searches.size(); // for loop: total site/search combos 
-  int limit = searches.size();
-  string outfile = name + ".csv";             // set file name for this iteration
-  ofstream outputFile(outfile.c_str());       // initialize file
-  if (outputFile.is_open()) {
-    //size_t j = 0;
-    //outputFile << queue_word_counts.back().first << " "<<queue_word_counts.back().second<<endl;
-    //queue_word_counts.pop();
-    for (int i = 0; i < limit; i++) {         // for each site/ word combo
-        //not writing to file
-        string tmpWord = queue_word_counts.front().first; 
-        int tmpCount = queue_word_counts.front().second;
-        queue_word_counts.pop(); // THIS LINE IS CAUSING SEGFAULT // pops the front
-        outputFile << tmpWord<<" "<<tmpCount <<endl;
-        /*outputFile << searches_counts[i] << " " << searches[j] << endl;
-        if (j == searches.size()-1) {
-	        j = 0;
-        }
-        else {
-	        j++;
-	    }*/
+    pthread_mutex_lock(&mutex);
+    while(queue_word_counts.empty()){ // nothing to write
+        pthread_cond_wait(&third_signal, &mutex);
     }
-    outputFile.close();
-  }
-  else {
-    fprintf(stderr,"config: couldn't write to %s: %s\n",outfile.c_str(),strerror(errno));
-  }
-  pthread_mutex_unlock(&mutex);
+
+    cout << "writing to output"<<endl;
+
+    //int limit = sites.size() * searches.size(); // for loop: total site/search combos 
+    int limit = searches.size();
+    string outfile = name + ".csv";             // set file name for this iteration
+    ofstream outputFile;       // initialize file
+    outputFile.open(outfile.c_str(), ios_base::app);    // open the file to append to it
+    if (outputFile.is_open()) {
+        //size_t j = 0;
+        //outputFile << queue_word_counts.back().first << " "<<queue_word_counts.back().second<<endl;
+        //queue_word_counts.pop();
+        for (int i = 0; i < limit; i++) {         // for each site/ word combo
+            //not writing to file
+            string tmpWord = queue_word_counts.front().first; 
+            int tmpCount = queue_word_counts.front().second;
+            queue_word_counts.pop(); // THIS LINE IS CAUSING SEGFAULT // pops the front
+            outputFile << tmpWord<<" "<<tmpCount <<endl;
+            /*outputFile << searches_counts[i] << " " << searches[j] << endl;
+              if (j == searches.size()-1) {
+              j = 0;
+              }
+              else {
+              j++;
+              }*/
+        }
+        outputFile.close();
+    }
+    else {
+        fprintf(stderr,"config: couldn't write to %s: %s\n",outfile.c_str(),strerror(errno));
+    }
+    pthread_mutex_unlock(&mutex);
 }
 
-void get_site(string site){
-  //cout << "inside get_site" << endl;
-  CURL *curl_handle;
-  CURLcode res;
-  
-  struct MemoryStruct chunk;
-  
-  chunk.memory = (char*)malloc(1);
-  chunk.size = 0;
-  curl_handle = curl_easy_init();  
-  
-  curl_easy_setopt(curl_handle, CURLOPT_URL, site.c_str()); //fetch site
-  curl_easy_setopt(curl_handle, CURLOPT_FOLLOWLOCATION, 1L);
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-  curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-  res = curl_easy_perform(curl_handle);
-  if(res != CURLE_OK) {
-    fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-  }
-  else{
-    //producer
+/*void initialize_output(string name) {
     pthread_mutex_lock(&mutex);
-    queue_data.push(chunk.memory);
-    //datacount++;
-    pthread_cond_broadcast(&producer_signal);
-    pthread_mutex_unlock(&mutex);
-  }
-  curl_easy_cleanup(curl_handle);
+    ofstream outputF*/
+
+
+void get_site(string site){
+    //cout << "inside get_site" << endl;
+    CURL *curl_handle;
+    CURLcode res;
+
+    struct MemoryStruct chunk;
+
+    chunk.memory = (char*)malloc(1);
+    chunk.size = 0;
+    curl_handle = curl_easy_init();  
+
+    curl_easy_setopt(curl_handle, CURLOPT_URL, site.c_str()); //fetch site
+    curl_easy_setopt(curl_handle, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+    res = curl_easy_perform(curl_handle);
+    if(res != CURLE_OK) {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    }
+    else{
+        //producer
+        pthread_mutex_lock(&mutex);
+        queue_data.push(chunk.memory);
+        //datacount++;
+        pthread_cond_broadcast(&producer_signal);
+        pthread_mutex_unlock(&mutex);
+    }
+    curl_easy_cleanup(curl_handle);
 }
 
 void * get_site_name(void * args){
-  //consumer
-  cout << "inside get_site_name"<<endl;
-  pthread_mutex_lock(&mutex);
-  while(queue_sites.empty()){
-    pthread_cond_wait(&consumer_signal, &mutex);
-  }
-  string site = queue_sites.front(); //check that this shoudl be back and not top
-  queue_sites.pop();
-  pthread_mutex_unlock(&mutex);
-  get_site(site);
-  return 0;
+    //consumer
+    cout << "inside get_site_name"<<endl;
+    pthread_mutex_lock(&mutex);
+    while(queue_sites.empty()){
+        pthread_cond_wait(&consumer_signal, &mutex);
+    }
+    string site = queue_sites.front(); //check that this shoudl be back and not top
+    queue_sites.pop();
+    pthread_mutex_unlock(&mutex);
+    get_site(site);
+    return 0;
 }
 
 void * find_words(void * args){
-  cout << "inside find_words"<<endl;
-  //consumer
-  pthread_mutex_lock(&mutex);
-  while(queue_data.empty()){
-    pthread_cond_wait(&producer_signal, &mutex);
-  }
-  string s = queue_data.back();
-  queue_data.pop();
-  pthread_cond_broadcast(&consumer_signal);
-  //datacount--;
-  pthread_mutex_unlock(&mutex);
-
-  int count = 0;
-  string word;
-  //searches_counts.clear();
-  //LOCK
-  pthread_mutex_lock(&mutex); //might not need?
-  queue_word_counts.empty();
-  for (size_t i = 0; i < queue_search.size(); i++) {
-    count = 0;
-    word = searches[i];
-    size_t n = s.find(word, 0);
-    while(n != string::npos){
-      count++;
-      n = s.find(word, n+1);
+    cout << "inside find_words"<<endl;
+    //consumer
+    pthread_mutex_lock(&mutex);
+    while(queue_data.empty()){
+        pthread_cond_wait(&producer_signal, &mutex);
     }
-    //searches_counts.push_back(count);
-    queue_word_counts.push(make_pair(word, count));
-    cout << "word: "<<word<<" count: "<<count<<endl;
-    //cout << queue_word_counts.back().first << " "<<queue_word_counts.back().second<<endl;
-  }
-  pthread_cond_broadcast(&third_signal);
-  pthread_mutex_unlock(&mutex);
-  stringstream ss;
-  ss << numFile;
-  string str = ss.str();
-  write_to_output(str);
+    string s = queue_data.back();
+    queue_data.pop();
+    pthread_cond_broadcast(&consumer_signal);
+    //datacount--;
+    pthread_mutex_unlock(&mutex);
 
-  return 0;
+    int count = 0;
+    string word;
+    //searches_counts.clear();
+    //
+    pthread_mutex_lock(&mutex); //LOCK: might not need?
+    queue_word_counts.empty();
+    cout << "new site: " << endl;
+    
+    // get file name
+    stringstream ss;
+    ss << numFile;
+    string str = ss.str();
+
+    // for each word in search queue, store the number of occurrences
+    for (size_t i = 0; i < queue_search.size(); i++) {
+        count = 0;
+        word = searches[i];
+        size_t n = s.find(word, 0);
+        while(n != string::npos){
+            count++;
+            n = s.find(word, n+1);
+        }
+        //searches_counts.push_back(count);
+        queue_word_counts.push(make_pair(word, count));
+        cout << "word: "<<word<<" count: "<<count<<endl;
+        //cout << queue_word_counts.back().first << " "<<queue_word_counts.back().second<<endl;
+    }
+
+    pthread_cond_broadcast(&third_signal);
+    pthread_mutex_unlock(&mutex); //UNLOCK
+    write_to_output(str);
+
+    return 0;
 }
 
 
 void push_sites_to_queue() {
-  pthread_mutex_lock(&mutex);
-  for (size_t i = 0; i < sites.size(); i++) {
-    cout << "pushing site to queue" << endl;
-    queue_sites.push(sites[i]);
-  }
-  pthread_cond_broadcast(&consumer_signal);
-  pthread_mutex_unlock(&mutex);
+    pthread_mutex_lock(&mutex);
+    for (size_t i = 0; i < sites.size(); i++) {
+        cout << "pushing site to queue" << endl;
+        queue_sites.push(sites[i]);
+    }
+    pthread_cond_broadcast(&consumer_signal);
+    pthread_mutex_unlock(&mutex);
 }
--- main.cpp
+++ main.cpp
@@ -7,50 +7,55 @@
 #define MAX_THREAD 8
 
 int main(int argc, char *argv[]){
-  curl_global_init(CURL_GLOBAL_ALL);
-  pthread_t *threads_f;
-  pthread_t *threads_p;
+    // initialize variables
+    curl_global_init(CURL_GLOBAL_ALL);
+    pthread_t *threads_f;
+    pthread_t *threads_p;
+    Config run;
 
-  Config run;
-  if (argc != 2) {
-  	cout << argv[0] << ": Incorrect arguments!" << endl;
-  	cout << "usage: " << argv[0] << " <configfile>" << endl;
-  	exit(1);
-  }
-  string fname = argv[1];
-  run.parse_input_file(fname);
-  int num_fetch = run.get_fetch_threads();
-  int num_parse = run.get_parse_threads();
-  if ((num_fetch < 1) || (num_parse < 1) || (num_fetch > MAX_THREAD) || (num_parse > MAX_THREAD)) {
-	printf("The number of threads should be between 1 and %d.\n", MAX_THREAD);
-	exit(1);
-  }
-  //parse files
-  run.parse_search_file();
-  run.parse_site_file();
-  run.push_search_to_queue();
-
-  threads_f = (pthread_t *)malloc(num_fetch*sizeof(*threads_f));
-  threads_p = (pthread_t *)malloc(num_parse*sizeof(*threads_p)); 
-
-  //run every period_fetch
-  while(1){
-    numFile++;
-    //creating number of threads specified
-    for(int i=0; i<run.get_fetch_threads(); i++){
-      //cout << "creating fetch thread"<<endl;
-      pthread_create(&threads_f[i], NULL, get_site_name, NULL);
+    // check for correct arguments
+    if (argc != 2) {
+        cout << argv[0] << ": Incorrect arguments!" << endl;
+        cout << "usage: " << argv[0] << " <configfile>" << endl;
+        exit(1);
     }
-    for(int j=0; j<run.get_parse_threads(); j++){
-      //cout << "creating parse thread"<<endl;
-      pthread_create(&threads_p[j], NULL, find_words, NULL);
+
+    // get info from files
+    string fname = argv[1]; // config file
+    run.parse_input_file(fname);    
+    int num_fetch = run.get_fetch_threads();
+    int num_parse = run.get_parse_threads();
+    // thread count error-checking
+    if ((num_fetch < 1) || (num_parse < 1) || (num_fetch > MAX_THREAD) || (num_parse > MAX_THREAD)) {
+        printf("The number of threads should be between 1 and %d.\n", MAX_THREAD);
+        exit(1);
     }
-    //populate sites queue
-    push_sites_to_queue();
-    sleep(run.get_period_fetch());
-  }
-  //free(threads_f);
-  //free(threads_p);
-  curl_global_cleanup();
-  return 1;
+
+    //parse files
+    run.parse_search_file();
+    run.parse_site_file();
+    run.push_search_to_queue();
+
+    threads_f = (pthread_t *)malloc(num_fetch*sizeof(*threads_f));
+    threads_p = (pthread_t *)malloc(num_parse*sizeof(*threads_p)); 
+
+    //run every period_fetch
+    while(1){
+        numFile++;
+        //creating number of threads specified
+        for(int i=0; i<run.get_fetch_threads(); i++){
+            //cout << "creating fetch thread"<<endl;
+            pthread_create(&threads_f[i], NULL, get_site_name, NULL);
+        }
+        for(int j=0; j<run.get_parse_threads(); j++){
+            //cout << "creating parse thread"<<endl;
+            pthread_create(&threads_p[j], NULL, find_words, NULL);
+        }
+        //populate sites queue
+        push_sites_to_queue();
+        sleep(run.get_period_fetch());
+    }
+
+    curl_global_cleanup();
+    return 0;
 }
